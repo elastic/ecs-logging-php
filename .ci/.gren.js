22c353f7c371f4ca2a8d40f72630d8dcef9095cc
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-02-08 11:24:11 +0000

    [CI] automate release process

---
+++ .ci/.gren.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "username": "elastic",
+    "repo": "ecs-logging-php",
+    "dataSource": "prs",
+    "ignoreIssuesWith": [
+      "automation",
+      "ci",
+      "developer only"
+    ],
+    "groupBy": {
+        "Breaking changes": ["breaking"],
+        "Bug fixes": ["bug", "fix"],
+        "Features": ["enhancement", "feature", "feat"]
+    },
+    "template": {
+        commit: ({ message, url, author, name }) => `- [${message}](${url}) - ${author ? `@${author}` : name}`,
+        issue: "- {{labels}} {{name}} [{{text}}]({{url}})",
+        label: "[**{{label}}**]",
+        noLabel: "closed",
+        changelogTitle: "# Changelog\n\n",
+        release: "## {{release}} ({{date}})\n{{body}}",
+        releaseSeparator: "\n---\n\n",
+        group: function (placeholders) {
+          return '\n#### ' + placeholders.heading + '\n';
+        }
+    }
+}
