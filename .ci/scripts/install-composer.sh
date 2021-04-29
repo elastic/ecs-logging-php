8521436072dd157d5fc13694c00d9a1303eae034
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2021-04-29 20:30:48 +0300

    Updated composer version used by CI tests to 2.0.13

--- .ci/scripts/install-composer.sh
+++ .ci/scripts/install-composer.sh
@@ -3,7 +3,7 @@ set -xueo pipefail
 
 export COMPOSER_ALLOW_SUPERUSER=1
 export COMPOSER_HOME=/tmp
-export COMPOSER_VERSION=1.9.1
+export COMPOSER_VERSION=2.0.13
 
 ## See https://github.com/composer/docker/blob/0dbcef7c67fcf1ce30c66ff42759f6af58b4960c/1.9/Dockerfile
 curl --silent --fail --location --retry 3 --output /tmp/installer.php --url https://raw.githubusercontent.com/composer/getcomposer.org/cb19f2aa3aeaa2006c0cd69a7ef011eb31463067/web/installer; \
